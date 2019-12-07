f8c4fbd67c918b9af8604574a1e4952361e4845a
BarenAD <baren1998@gmail.com>
2019-12-07 18:06:16 +0700

    Add code print all

--- Main_HomeWork4.php
+++ Main_HomeWork4.php
@@ -12,3 +12,17 @@ $BDServices = new BDServicesForHomeWork4($Connection);
 
 $Homework4NewsDB = new NewsDB($BDServices);
 $Homework4AnnounceDB = new AnnounceDB($BDServices);
+
+$CommonArray = array();
+
+foreach ($Homework4NewsDB->get_all() as $class){
+    $CommonArray[] = $class;
+}
+foreach ($Homework4AnnounceDB->get_all() as $class){
+    $CommonArray[] = $class;
+}
+foreach ($CommonArray as $class){
+    echo "SOURCE :: " . $class->getSource() . PHP_EOL;
+    echo "SOURCE :: " . $class->getContent() . PHP_EOL;
+    echo "------------------------------------" . PHP_EOL;
+}
